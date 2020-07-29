0423d199d34c2821f0eaf93097fec51e419fb0b5
tanishqshek <tanishq.s@somaiya.edu>
2020-07-29 22:48:58 +0530

    Search Bar

--- js/scripts.js
+++ js/scripts.js
@@ -20,4 +20,46 @@ $(document).ready(function(){
     $('#reserveModalButton').click(function(){
         $('#reserveModal').modal('show');
     });
+
+    $('#registerModalButton').click(function(){
+        $('#registerModal').modal('show');
+    });
+
+    $('#101ModalButton').click(function(){
+      $('#101Modal').modal('show');
+  });
+
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+    $("#famMembers").change(function() {
+        if ($(this).val() == "1") {
+          $('#details1').show();
+          $('#details-2').hide();
+          $('#details-3').hide();
+        } 
+        else if ($(this).val() == "2") {
+            $('#details-2').show();
+            $('#details1').hide();
+            $('#details-3').hide();
+          } 
+        else if ($(this).val() == "3") {
+        $('#details-3').show();
+        $('#details-2').hide();
+        $('#details1').hide();
+        } 
+        else {
+          $('#details1').hide();
+          $('#details-2').hide();
+          $('#details-3').hide();
+        //   $('#details-2').hide();
+        //   $('#details-3').hide();
+        }
+      });
+
+    $("#famMembers").trigger("change");
 })
